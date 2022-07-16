1ea520e252e8887d1339fe6028c807e3e2c31a3f
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-07-16 19:49:07 +0800

    last push bago mag out

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,7 +2,21 @@ import React from 'react'
 import './Services.css'
 
 const Services = () => {
-  return <div>Services</div>
+  return (
+    <div className="services section">
+      <div className="container--sm">
+        <h2>SERVICES</h2>
+
+        <div className="services__wrapper">
+          <div className="service">Front-End Development</div>
+
+          <div className="service">Back-End Development</div>
+
+          <div className="service">Database Management</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Services
