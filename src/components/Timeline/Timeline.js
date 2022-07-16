a439620a248c8907634f3f1ee88ca81eb4c4ef8a
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-07-16 19:43:04 +0800

    Added CSS to timeline section

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -18,27 +18,28 @@ const Timeline = () => {
           </div>
 
           <div className="timeline__card">
-            <h4>COLLEGE</h4>
-            <p className="">City College of Angeles</p>
+            <h4>SENIOR HIGH SCHOOL</h4>
+            <p className="">Angeles City Science High School</p>
             <p className="">
-              <i>Bachelor of Science in Information Systems</i>
+              <i>STEM</i>
             </p>
-            <p>2018 - 2022</p>
+            <p>2016 - 2018</p>
           </div>
 
           <div className="timeline__card">
-            <h4>COLLEGE</h4>
-            <p className="">City College of Angeles</p>
+            <h4>JUNIOR HIGH SCHOOL</h4>
+            <p className="">Francisco G. Nepomuceno Memorial High School</p>
             <p className="">
-              <i>Bachelor of Science in Information Systems</i>
+              <i>Cookery</i>
             </p>
-            <p>2018 - 2022</p>
+            <p>2014 - 2018</p>
           </div>
         </div>
 
         <div className="timeline__texts">
           <h2>TIMELINE</h2>
-          <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit."</p>
+          <p>"For nothing will be impossible with God."</p>
+          <p>— Luke 1:37</p>
         </div>
       </div>
     </div>
