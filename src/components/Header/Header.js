3bb62195b427f319b3bdda1bbd2f307b18496603
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-07-17 19:39:59 +0800

    Added JavaScript

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './Header.css'
 
 const Header = () => {
+  const [isHeaderActive, setIsHeaderActive] = useState(false)
+  const headerRef = useRef(null)
+
+  const handleHeaderToggle = () => {
+    setIsHeaderActive(!isHeaderActive)
+
+    if (isHeaderActive) {
+      document.body.style.overflowY = 'scroll'
+    } else {
+      document.body.style.overflowY = 'hidden'
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    setIsHeaderActive(false)
+
+    if (window.innerWidth > 1100) {
+      headerRef.current.style.height = '120px'
+    } else {
+      headerRef.current.style.height = '100px'
+    }
+  })
+
+  window.addEventListener('scroll', () => {
+    if (headerRef && headerRef.current) {
+      if (window.scrollY > 120) {
+        headerRef.current.style.backgroundColor = '#262626'
+        headerRef.current.style.height = '100px'
+      } else {
+        headerRef.current.style.backgroundColor = 'transparent'
+
+        if (window.innerWidth > 1100) {
+          headerRef.current.style.height = '120px'
+        }
+      }
+    }
+  })
+
   return (
-    <header className="header">
+    <header
+      className={isHeaderActive ? 'header active' : 'header'}
+      ref={headerRef}
+    >
       <div className="container--lg">
         <a href="" className="header__logo">
           <img src="/images/logo.png" alt="" />
         </a>
 
-        <div className="header__menu">
+        <div className="header__menu" onClick={handleHeaderToggle}>
           <span></span>
           <span></span>
           <span></span>
