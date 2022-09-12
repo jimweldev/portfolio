11d31af8acf27e3c2b5af62cc1de242e96e5aadf
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-09-12 20:20:06 +0800

    Changed images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,35 +3,35 @@ import { motion } from 'framer-motion'
 import './About.css'
 
 const About = () => {
-  return (
-    <div className="about section" id="about">
-      <div className="container--sm">
-        <motion.div
-          className="about__image"
-          initial={{ x: '100%' }}
-          whileInView={{ x: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <img src="/images/about.jpg" alt="" />
-        </motion.div>
+	return (
+		<div className="about section" id="about">
+			<div className="container--sm">
+				<motion.div
+					className="about__image"
+					initial={{ x: '100%' }}
+					whileInView={{ x: 0 }}
+					transition={{ duration: 0.3 }}
+				>
+					<img src="/images/about2.jpg" alt="" />
+				</motion.div>
 
-        <div className="about__texts">
-          <h2>WHO I AM</h2>
-          <p>
-            My name is Jimwel Dizon. I am 22 years old from Angeles City,
-            Pampanga. I have been living with my parents and two siblings. I
-            have completed my Bachelor of Science in Information Systems degree
-            from City College of Angeles.
-          </p>
-          <p>
-            I am interested in programming, games, and puzzles. Apart from these
-            hobbies, I love to hang out with my close friends. My main goal is
-            to become a Full-stack Web Developer.
-          </p>
-        </div>
-      </div>
-    </div>
-  )
+				<div className="about__texts">
+					<h2>WHO I AM</h2>
+					<p>
+						My name is Jimwel Dizon. I am 22 years old from Angeles City,
+						Pampanga. I have been living with my parents and two siblings. I
+						have completed my Bachelor of Science in Information Systems degree
+						from City College of Angeles.
+					</p>
+					<p>
+						I am interested in programming, games, and puzzles. Apart from these
+						hobbies, I love to hang out with my close friends. My main goal is
+						to become a Full-stack Web Developer.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default About
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,108 +5,108 @@ import { FaCertificate } from 'react-icons/fa'
 import './Hero.css'
 
 const Hero = () => {
-  const hours = new Date().getHours()
-  let greetings
+	const hours = new Date().getHours()
+	let greetings
 
-  if (hours < 12) {
-    greetings = 'Good Morning!'
-  } else if (hours < 18) {
-    greetings = 'Good Afternoon!'
-  } else {
-    greetings = 'Good Evening!'
-  }
+	if (hours < 12) {
+		greetings = 'Good Morning!'
+	} else if (hours < 18) {
+		greetings = 'Good Afternoon!'
+	} else {
+		greetings = 'Good Evening!'
+	}
 
-  return (
-    <div className="hero section" id="home">
-      <div className="container">
-        <div className="hero__texts">
-          <p>{greetings}</p>
-          <h1>
-            I'm <span>Jimwel Dizon</span>
-          </h1>
-          <p>An aspiring Web Developer</p>
-        </div>
+	return (
+		<div className="hero section" id="home">
+			<div className="container">
+				<div className="hero__texts">
+					<p>{greetings}</p>
+					<h1>
+						I'm <span>Jimwel Dizon</span>
+					</h1>
+					<p>An aspiring Web Developer</p>
+				</div>
 
-        <div className="hero__social-medias">
-          <motion.a
-            href="https://www.facebook.com/jimweldizon2"
-            target="_blank"
-            whileHover={{ scale: 1.2, rotate: 360 }}
-            whileTap={{
-              scale: 0.8,
-              rotate: -360,
-              borderRadius: '100%',
-            }}
-            transition={{ duration: 0.3 }}
-          >
-            <BsFacebook />
-          </motion.a>
-          <motion.a
-            href="https://www.linkedin.com/in/jimwel-marius-dizon-03b383225/"
-            target="_blank"
-            whileHover={{ scale: 1.2, rotate: 360 }}
-            whileTap={{
-              scale: 0.8,
-              rotate: -360,
-              borderRadius: '100%',
-            }}
-            transition={{ duration: 0.3 }}
-          >
-            <BsLinkedin />
-          </motion.a>
-          <motion.a
-            href="https://github.com/jimweldev"
-            target="_blank"
-            whileHover={{ scale: 1.2, rotate: 360 }}
-            whileTap={{
-              scale: 0.8,
-              rotate: -360,
-              borderRadius: '100%',
-            }}
-            transition={{ duration: 0.3 }}
-          >
-            <BsGithub />
-          </motion.a>
-          <motion.a
-            href="/certificates"
-            target="_blank"
-            whileHover={{ scale: 1.2, rotate: 360 }}
-            whileTap={{
-              scale: 0.8,
-              rotate: -360,
-              borderRadius: '100%',
-            }}
-            transition={{ duration: 0.3 }}
-          >
-            <FaCertificate />
-          </motion.a>
-        </div>
+				<div className="hero__social-medias">
+					<motion.a
+						href="https://www.facebook.com/jimweldizon2"
+						target="_blank"
+						whileHover={{ scale: 1.2, rotate: 360 }}
+						whileTap={{
+							scale: 0.8,
+							rotate: -360,
+							borderRadius: '100%',
+						}}
+						transition={{ duration: 0.3 }}
+					>
+						<BsFacebook />
+					</motion.a>
+					<motion.a
+						href="https://www.linkedin.com/in/jimwel-marius-dizon-03b383225/"
+						target="_blank"
+						whileHover={{ scale: 1.2, rotate: 360 }}
+						whileTap={{
+							scale: 0.8,
+							rotate: -360,
+							borderRadius: '100%',
+						}}
+						transition={{ duration: 0.3 }}
+					>
+						<BsLinkedin />
+					</motion.a>
+					<motion.a
+						href="https://github.com/jimweldev"
+						target="_blank"
+						whileHover={{ scale: 1.2, rotate: 360 }}
+						whileTap={{
+							scale: 0.8,
+							rotate: -360,
+							borderRadius: '100%',
+						}}
+						transition={{ duration: 0.3 }}
+					>
+						<BsGithub />
+					</motion.a>
+					<motion.a
+						href="/certificates"
+						target="_blank"
+						whileHover={{ scale: 1.2, rotate: 360 }}
+						whileTap={{
+							scale: 0.8,
+							rotate: -360,
+							borderRadius: '100%',
+						}}
+						transition={{ duration: 0.3 }}
+					>
+						<FaCertificate />
+					</motion.a>
+				</div>
 
-        <div className="hero__image">
-          <img src="/images/hero.png" alt="" />
-        </div>
-      </div>
+				<div className="hero__image">
+					<img src="/images/hero2.png" alt="" />
+				</div>
+			</div>
 
-      <div className="hero__overlay">
-        <p>
-          JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
-          JIMWEL DIZON JIMWEL DIZON
-        </p>
-        <p>
-          DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
-          JIMWEL DIZON JIMWEL
-        </p>
-        <p>
-          JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
-          JIMWEL DIZON JIMWEL DIZON
-        </p>
-        <p>
-          DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
-          JIMWEL DIZON JIMWEL
-        </p>
-      </div>
-    </div>
-  )
+			<div className="hero__overlay">
+				<p>
+					JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
+					JIMWEL DIZON JIMWEL DIZON
+				</p>
+				<p>
+					DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
+					JIMWEL DIZON JIMWEL
+				</p>
+				<p>
+					JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
+					JIMWEL DIZON JIMWEL DIZON
+				</p>
+				<p>
+					DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON JIMWEL DIZON
+					JIMWEL DIZON JIMWEL
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default Hero
